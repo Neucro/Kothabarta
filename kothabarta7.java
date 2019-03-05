26ab86a1c335c7c98677e4d6f098591af258c740
Neucro <sajidcsebu@gmail.com>
2019-03-05 13:39:42 +0600

    Kothabarta

---
+++ kothabarta7.java
@@ -0,0 +1,165 @@
+import java.util.*;
+//Author name: Sajid
+public class kothabarta7//a.compareTo(b)
+{
+  public static void main(String[]args)
+  {
+    Scanner k=new Scanner (System.in);
+    System.out.println("Hi...");
+    String x;
+    System.out.println("How are you...?");
+    x=k.nextLine();
+    System.out.println("okay you are"+" "+x+" "+":D");
+    System.out.println("What are you doing now?");
+    String y;
+    y=k.nextLine();
+    System.out.println("okay, I got it"+" "+"you are"+" "+y+" "+"O:)");
+    System.out.println("How is going on everything?");
+    String z;
+    z=k.nextLine();
+    System.out.println("ohhh... I see...");
+    System.out.println("Are you Married? No need to explain anything, just give answer by saying yes or no");
+    String a;
+    String b="yes";
+    String c="no";
+    a=k.nextLine();
+    if((a.equals(b))||(a.equals(c)))
+    {
+      if(a.equals(b))
+      {
+      System.out.println("oh that's great");
+      }
+      else
+      {
+        if(a.equals(c))
+      {
+        System.out.println("why?");
+        
+        String v=k.nextLine();
+        System.out.println("oh I see...");
+        }
+      }
+        
+      }
+    //Sajid
+    else
+    {
+      System.out.println("I told you to give your answer by telling me just yes or no, I didn't tell you to say anything rather than it, so plase giving me answer by just saying yes or no");
+      String d;
+      d=k.nextLine();
+      if((d.equals(b))||(d.equals(c)))
+    {
+      if(d.equals(b))
+      {
+      System.out.println("oh that's great");
+      }
+      if(d.equals(c))
+      {
+        System.out.println("why?");
+        String w;
+        w=k.nextLine();
+        System.out.println("oh I see...");
+      }
+      
+      }
+      }
+    
+    System.out.println("How many brothers and sisters do you have?");
+    String e;
+    e=k.nextLine();
+    System.out.println("I got it");
+    System.out.println("Kindly give your email ID in the below box");
+    String f;
+    f=k.nextLine();
+    System.out.println("Kindly give your email ID in the below box please...");
+    String g;
+    g=k.nextLine();
+    if(f.equals(g))
+    {
+      System.out.println("Thank you");
+    }
+    else
+    {
+      System.out.println(":/");
+    }
+    System.out.println("What is your favourite colour?");
+    String h=k.nextLine();
+    System.out.println("oh that's great... your favourite colour is"+" "+h+" "+":>");
+    System.out.println("Guess...what is my favourite colour?");
+    String i="white";//Sajid
+    String j=k.nextLine();
+    if(i.equals(j))
+    {
+      System.out.println("yeah...you are a genius...you are absolutely right");
+    }
+    else
+    {
+      System.out.println("No, you are wrong");
+    }
+    System.out.println("Do you know... who... are.... you.... ???");
+    String v=k.nextLine();
+    System.out.println("hmmmmmm....."+" "+"you are"+" "+v+" "+":P");
+    System.out.println("okay, now tell me about your interest");
+    String u=k.nextLine();
+    System.out.println(u+".... :p ");
+    System.out.println("do you know, who wrote the Anthem of Bangladesh");
+    String m="Robindronath Tagore";
+    String n="Rabindranath Tagore";
+    String o="Rabindranath Thakur";
+    String p="Robindronath Thakur";
+    String q="Rabindronath Thakur";
+    String r="Rabindronath Tagore";
+    String s=k.nextLine();
+    if((s.equals(m))||(s.equals(n))||(s.equals(o))||(s.equals(p))||(s.equals(q))||(s.equals(r)))
+    {
+      if(s.equals(m))
+      {
+        System.out.println("yeah... you are right,but there is spelling mistake of name,the correct spelling of his name was Rabindranath Tagore");
+      }
+      if(s.equals(n))
+      {
+        System.out.println("yeah...you are 100% right and you wrote the correct spelling of his name :)");
+      }
+      if(s.equals(o))
+      {
+        System.out.println("yeah... you are right,but there is spelling mistake of name,the correct spelling of his name was Rabindranath Tagore");
+      }
+      if(s.equals(p))
+      {
+        System.out.println("yeah... you are right,but there is spelling mistake of name,the correct spelling of his name was Rabindranath Tagore");
+      }
+      if(s.equals(q))
+      {
+        System.out.println("yeah... you are right,but there is spelling mistake of name,the correct spelling of his name was Rabindranath Tagore");
+      }
+      if(s.equals(r))
+      {
+        System.out.println("yeah... you are right,but there is spelling mistake of name,the correct spelling of his name was Rabindranath Tagore");
+      }
+    }
+    else
+    {
+      System.out.println("I am sorry to say that you have wrong idea :(");
+    }
+    System.out.println("heyyyyy......buddy....are you feeling bore to chat with me?");
+    String t=k.nextLine();
+    System.out.println("oooo......");
+    System.out.println(" ");
+    System.out.println("I am in a little bit hurry, so bye..... take care... :) ");
+  }
+}
+        
+        
+
+  
+
+    
+  
+
+  
+
+    
+    
+  
+
+   
